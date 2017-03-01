81bb5d8c855792342dd41adb6f326bba687d9e75
Jess <jess.a.burnett@gmail.com>
2017-03-01 06:06:18 +0000

    latest dev

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ function toggleIntroVideo (){
     event.preventDefault();
     var introVideoPlaceholder = document.getElementById("intro-video-still");
     var introVideo = document.getElementById("intro-video");
+    var videoUrl = "https://player.vimeo.com/video/141448733?autoplay=1";
+    
+    console.log(videoUrl);
+    
+    document.getElementById('vimeo').setAttribute("src", videoUrl);
     
     if (introVideo.style.display === 'block') {
         introVideo.style.display = 'none';
@@ -14,4 +19,5 @@ function toggleIntroVideo (){
     } else {
         introVideoPlaceholder.style.display = 'none';
     }
+    
 }
